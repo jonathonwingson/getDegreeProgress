02bd281948448e2d1329cac781f09f9984050a41
jWing <78874755+jonathonwingson@users.noreply.github.com>
2023-07-27 10:35:50 -0400

    Update getCourseProgress.js

--- getCourseProgress.js
+++ getCourseProgress.js
@@ -1,13 +1,14 @@
 
 const studentCourseCodes = [
-    'COMP1600', 'COMP1601', 'COMP1602',
-    'COMP1603', 'COMP2601', 'COMP2602',
-    'COMP2603', 'COMP2604', 'COMP2605',
-    'COMP2606', 'COMP2611', 'COMP3601',
-    'COMP3603', 'COMP3609', 'COMP3613',
-    'FOUN1101', 'FOUN1105', 'FOUN1301',
-    'INFO1600', 'INFO1601', 'INFO2602',
-    'INFO2604', 'MATH1115', 'MATH2250'];
+    'COMP1600', 'COMP1601', 'FOUN1101', 'INFO1600', 'MATH1115',
+    'COMP1602', 'COMP1603', 'COMP1604', 'FOUN1105', 'INFO1601',
+    'COMP2605', 'COMP2611',
+    'COMP2603', 'COMP2604', 'COMP2606','INFO2602', 'INFO2604',
+    'COMP2601', 'FOUN1301',
+    'COMP2602', 'COMP3602', 'COMP3603', 'COMP3613', 'MATH2250',
+    'COMP3601', 'COMP3609', 'INFO3604', 'INFO3608', 'INFO3611',
+    // 'INFO3610' 
+ ];
 
 const courses = [
     { courseCode: "COMP1600", courseTitle: "Introduction to Computing Concepts", credits: 3.0, level: "I", semester: "1", createdAt: "2023-07-03 06:33:08.47259+00", updatedAt: "2023-07-03 06:33:08.47259+00" },
@@ -53,23 +54,109 @@ const courses = [
     { courseCode: "FOUN1301", courseTitle: "Law Governance Economy and Society", credits: 3, level: "I", semester: "2", description: "desc", createdAt: "2023-07-09 01:46:50.627+00", updatedAt: "2023-07-09 01:46:50.627+00" }
 ];
 
+const programmeCourses = [
+    { programmeId: 1, courseCode: "COMP1600", typeId: 1},
+    { programmeId: 1, courseCode: "COMP1601", typeId: 1},
+    { programmeId: 1, courseCode: "COMP1602", typeId: 1},
+    { programmeId: 1, courseCode: "COMP1603", typeId: 1},
+    { programmeId: 1, courseCode: "COMP1604", typeId: 1},
+    { programmeId: 1, courseCode: "COMP2601", typeId: 2},
+    { programmeId: 1, courseCode: 'COMP2602', typeId: 2},
+    { programmeId: 1, courseCode: "COMP2603", typeId: 2},
+    { programmeId: 1, courseCode: "COMP2604", typeId: 2},
+    { programmeId: 1, courseCode: "COMP2605", typeId: 2},
+    { programmeId: 1, courseCode: "COMP2606", typeId: 2},
+    { programmeId: 1, courseCode: "COMP2611", typeId: 2},
+    { programmeId: 1, courseCode: "COMP3601", typeId: 3},
+    { programmeId: 1, courseCode: "COMP3602", typeId: 3},
+    { programmeId: 1, courseCode: "COMP3603", typeId: 3},
+    { programmeId: 1, courseCode: "COMP3605", typeId: 5},
+    { programmeId: 1, courseCode: "COMP3606", typeId: 5},
+    { programmeId: 1, courseCode: "COMP3607", typeId: 5},
+    { programmeId: 1, courseCode: 'COMP3608', typeId: 5},
+    { programmeId: 1, courseCode: "COMP3609", typeId: 5},
+    { programmeId: 1, courseCode: "COMP3610", typeId: 5},
+    { programmeId: 1, courseCode: "COMP3611", typeId: 5},
+    { programmeId: 1, courseCode: "COMP3612", typeId: 5},
+    { programmeId: 1, courseCode: "COMP3613", typeId: 3},
+    { programmeId: 1, courseCode: "FOUN1101", typeId: 6},
+    { programmeId: 1, courseCode: "FOUN1105", typeId: 6},
+    { programmeId: 1, courseCode: "FOUN1301", typeId: 6},
+    { programmeId: 1, courseCode: "INFO1600", typeId: 1},
+    { programmeId: 1, courseCode: "INFO1601", typeId: 1},
+    { programmeId: 1, courseCode: "INFO2602", typeId: 2},
+    { programmeId: 1, courseCode: 'INFO2604', typeId: 2},
+    { programmeId: 1, courseCode: "INFO2605", typeId: 5},
+    { programmeId: 1, courseCode: "INFO3600", typeId: 5},
+    { programmeId: 1, courseCode: "INFO3604", typeId: 3},
+    { programmeId: 1, courseCode: "INFO3605", typeId: 5},
+    { programmeId: 1, courseCode: "INFO3606", typeId: 5},
+    { programmeId: 1, courseCode: "INFO3607", typeId: 5},
+    { programmeId: 1, courseCode: "INFO3608", typeId: 5},
+    { programmeId: 1, courseCode: "INFO3609", typeId: 5},
+    { programmeId: 1, courseCode: "INFO3610", typeId: 5},
+    { programmeId: 1, courseCode: "INFO3611", typeId: 5},
+    { programmeId: 1, courseCode: 'MATH1115', typeId: 1},
+    { programmeId: 1, courseCode: "MATH2250", typeId: 2},
+];
+
+const programmeCreditRequirements = [
+    {programmeId: 1, Level1Core: 24, Level2Core: 30, Level3Core: 15, AdvanceCore: 0, AdvanceElective: 15, Foundation: 9, total: 93}
+];
+
+// const programmeCreditRequirements = [
+//     {programmeId: 1, Level1Core: 24-CS/IT, Level2Core: 30-CS/IT, Level3Core: 15-CS/IT, AdvanceCore: 0, AdvanceElective: 15-CS/IT, Foundation: 9, total: 93}
+// ];
+
 let degreeProgress =[];
 
-function getDegreeProgress(studentCourseCodes, courses) {
+function getDegreeProgress(programmeId, studentCourseCodes, courses, programmeCreditRequirements, programmeCourses) {
 
     let totalCredits = 0;
-    let studentCoursesData = [];
+    let completedCourses = [];
+ 
+    let programmeCreditRequirement = programmeCreditRequirements.find((pcr) => pcr.programmeId === programmeId);
 
     // for each student's course, display the course and check credit worth and sum it. 
     for (let i = 0; i < studentCourseCodes.length; i++) {
 
         // Find the corresponding course object in the Courses array
         let course = courses.find((c) => c.courseCode === studentCourseCodes[i]);
+        // Find the corresponding programmeCourse object in the ProgrammeCourses array
+        let programmeCourse = programmeCourses.find((c) => c.courseCode === studentCourseCodes[i]);
 
-        if (course) {
+        if (programmeCourse) {
+            courseCode = programmeCourse.courseCode;
+            type = programmeCourse.typeId;
 
-            totalCredits += course.credits;
-            studentCoursesData.push(course);
+            switch(type){
+                case 1: //type = Level1Core
+                    programmeCreditRequirement.Level1Core -= course.credits;
+                    totalCredits+=course.credits;
+                    break;
+                case 2: //type = Level2Core
+                    programmeCreditRequirement.Level2Core -= course.credits;
+                    totalCredits+=course.credits;
+                    break;
+                case 3: //type = Level3Core
+                    programmeCreditRequirement.Level3Core -= course.credits;
+                    totalCredits+=course.credits;
+                    break;
+                case 4: //type = AdvanceCore
+                    programmeCreditRequirement.AdvanceCore -= course.credits;
+                    totalCredits+=course.credits;
+                    break;
+                case 5: //type = AdvanceElective
+                    programmeCreditRequirement.AdvanceElective -= course.credits;
+                    totalCredits+=course.credits;
+                    break;
+                case 6: //type = Foundation
+                    programmeCreditRequirement.Foundation -= course.credits;
+                    totalCredits+=course.credits;
+                    break;
+            }
+            // totalCredits += course.credits;
+            completedCourses.push(course.courseCode);
         }
 
 
@@ -77,8 +164,10 @@ function getDegreeProgress(studentCourseCodes, courses) {
     }
     // if credit total less than the programme credit total display the remaining requirements
     degreeProgress = {
-        courseData: studentCoursesData ,
-         totalCredits: totalCredits 
+        remainingRequirements: programmeCreditRequirements,
+        completedCourses: completedCourses ,
+        totalCompletedCredits: totalCredits,
+        remainingCredits: programmeCreditRequirements.total-totalCredits 
     }
          
     
@@ -88,7 +177,8 @@ function getDegreeProgress(studentCourseCodes, courses) {
 }
 
 
-degreeProgress = getDegreeProgress(studentCourseCodes, courses);
+degreeProgress = getDegreeProgress(1, studentCourseCodes, courses, programmeCreditRequirements, programmeCourses );
 console.log("LOG::> Degree Progress: ");
-console.log("Course Data", degreeProgress.courseData);
-console.log("Total Credits", degreeProgress.totalCredits);
+console.log("Completed Courses: ", degreeProgress.completedCourses);
+console.log("Remaining Requirements: ", degreeProgress.remainingRequirements);
+console.log("Total Credits: ", degreeProgress.totalCompletedCredits);
