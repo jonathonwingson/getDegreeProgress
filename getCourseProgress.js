74ee3069a56e7a7c10dff157fd2d377fb54260d0
jWing <78874755+jonathonwingson@users.noreply.github.com>
2023-07-31 11:21:28 -0400

    Update getCourseProgress.js

--- getCourseProgress.js
+++ getCourseProgress.js
@@ -1,7 +1,7 @@
 
 // array to map typeIds to their names for a programme
 const typeTable = [
-    // programmeId 1
+    // // programmeId 1
     // { typeId: 1, type: "Level1Core" },
     // { typeId: 2, type: "Level2Core" },
     // { typeId: 3, type: "Level3Core" },
@@ -20,7 +20,7 @@ const typeTable = [
 
 // array of courseCodes the student has completed
 const studentCourseCodes = [
-    // porgrammeId 1
+    // // porgrammeId 1
     // 'COMP1600', 'COMP1601', 'FOUN1101', 'INFO1600', 'MATH1115',
     // 'COMP1602', 'COMP1603', 'COMP1604', 'FOUN1105', 'INFO1601',
     // 'COMP2605', 'COMP2611',
@@ -31,20 +31,16 @@ const studentCourseCodes = [
     // 'INFO3610' 
 
     // progrmmeId 2
-    "COMP1600", "COMP1601", "COMP1602", "COMP1603", //12cs/it core
+    "COMP1600", "COMP1601", "COMP1602", "COMP1603", //12L1 cs/it core
     "ACCT1002", "ACCT1003","ECON1001", "ECON1002",  //12A&Ecore
     "COMP2601", "COMP2602", "COMP2603", "COMP2604", "COMP2605", "COMP2611",//18 level2 cs core
     "MGMT2008", "MGMT2021", "MGMT2012", "MGMT2023", "MKTG2001",//15 advancecore
     "FOUN1101", "FOUN1105", "FOUN1301", //9 foun
 
-    "COMP3601", "COMP3602", "COMP3603", //12 advance elective
-    "COMP3605", "COMP3606", "ACCT2017",  "MKTG3000", "COMP2606",//15 advance elecetive Computer Science/Economics/Information Technology/Mathematics/Management
+    "COMP3601", "COMP3602", "COMP3603","COMP3605",  //12 advance elective
+    "COMP3606", "COMP3607", "INFO3610", "COMP2606",//15 advance elecetive Computer Science/Economics/Information Technology/Mathematics/Management
      
 
-    // "COMP3607", "COMP3608", "COMP3609", "COMP3610", "COMP3611",
-    // "COMP3612", "COMP3613", "INFO2602", "INFO2604", "INFO2605",
-    // "INFO3600", "INFO3605", "INFO3604", "INFO3606", "INFO3607",
-    // "INFO3608", "INFO3609", "INFO3610", "INFO3611",
 ];
 
 // all courses in the database
@@ -106,7 +102,7 @@ const courses = [
     { courseCode: "FOUN1301", courseTitle: "Law Governance Economy and Society", credits: 3, level: "I", semester: "2", description: "desc", createdAt: "2023-07-09 01:46:50.627+00", updatedAt: "2023-07-09 01:46:50.627+00" }
 ];
 
-// All courses in the programme 1
+// // All courses in the programme 1
 // const programmeCourses = [
 //     { programmeId: 1, courseCode: "COMP1600", typeId: 1 },
 //     { programmeId: 1, courseCode: "COMP1601", typeId: 1 },
@@ -163,8 +159,6 @@ const programmeCourses = [
     { programmeId: 2, courseCode: "COMP1603", typeId: 1 },
     { programmeId: 2, courseCode: "ECON1001", typeId: 1 },
     { programmeId: 2, courseCode: "ECON1002", typeId: 1 },
-    { programmeId: 2, courseCode: "FOUN1101", typeId: 6 },
-    { programmeId: 2, courseCode: "FOUN1105", typeId: 6 },
 
     { programmeId: 2, courseCode: "COMP2601", typeId: 2 },
     { programmeId: 2, courseCode: "COMP2602", typeId: 2 },
@@ -172,15 +166,19 @@ const programmeCourses = [
     { programmeId: 2, courseCode: "COMP2604", typeId: 2 },
     { programmeId: 2, courseCode: "COMP2605", typeId: 2 },
     { programmeId: 2, courseCode: "COMP2611", typeId: 2 },
+
     { programmeId: 2, courseCode: "MGMT2008", typeId: 4 },
     { programmeId: 2, courseCode: "MGMT2021", typeId: 4 },
-    { programmeId: 2, courseCode: "MKTG2001", typeId: 4 },
-    { programmeId: 2, courseCode: "FOUN1301", typeId: 6 },
-
-    { programmeId: 2, courseCode: "ACCT2017", typeId: 4 },
     { programmeId: 2, courseCode: "MGMT2012", typeId: 4 },
     { programmeId: 2, courseCode: "MGMT2023", typeId: 4 },
     { programmeId: 2, courseCode: "MKTG3000", typeId: 4 },
+    { programmeId: 2, courseCode: "MKTG2001", typeId: 4 },
+
+    { programmeId: 2, courseCode: "FOUN1301", typeId: 6 },
+    { programmeId: 2, courseCode: "FOUN1101", typeId: 6 },
+    { programmeId: 2, courseCode: "FOUN1105", typeId: 6 },
+
+    { programmeId: 2, courseCode: "ACCT2017", typeId: 4 },
 
     { programmeId: 2, courseCode: "COMP2606", typeId: 7 },
     { programmeId: 2, courseCode: "COMP3601", typeId: 7 },
@@ -208,11 +206,38 @@ const programmeCourses = [
     { programmeId: 2, courseCode: "INFO3610", typeId: 7 },
     { programmeId: 2, courseCode: "INFO3611", typeId: 7 },
 
+    { programmeId: 2, courseCode: "COMP2606", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3601", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3602", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3603", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3605", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3606", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3607", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3608", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3609", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3610", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3611", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3612", typeId: 8 },
+    { programmeId: 2, courseCode: "COMP3613", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO2602", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO2604", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO2605", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3600", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3605", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3604", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3606", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3607", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3608", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3609", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3610", typeId: 8 },
+    { programmeId: 2, courseCode: "INFO3611", typeId: 8 },
+
+
 ];
 
 // credit requirement for the programme
 const programmeCreditRequirements = [
-    // programme 1
+    // // programme 1
     // {
     //     programmeId: 1,
     //     creditRequirements: {
@@ -232,7 +257,6 @@ const programmeCreditRequirements = [
             Level1Core: 24,
             Level2Core: 18,
             AdvancedCore: 15,
-            AdvanceElective: 15,
             Foundation: 9,
             "AdvancedElectiveCS/IT": 12,
             "AdvancedElectiveCS/IT/E/MATH/M": 15,
@@ -249,26 +273,48 @@ function getDegreeProgress(programmeId, studentCourseCodes, courses, programmeCr
     let programmeCreditRequirement = programmeCreditRequirements.find((pcr) => pcr.programmeId === programmeId);
     let creditRequirements = programmeCreditRequirement.creditRequirements;
 
-    for (let i = 0; i < studentCourseCodes.length; i++) {
-        let course = courses.find((c) => c.courseCode === studentCourseCodes[i]);
-        let programmeCourse = programmeCourses.find((c) => c.courseCode === studentCourseCodes[i]);
+    for( let creditType in creditRequirements){
+        
+        let typeObj = typeTable.find((type) => type.type === creditType);
+        let typeId = typeObj ? typeObj.typeId : null;
+        //console.log(typeId);
 
-        if (programmeCourse) {
-
-            //let type = typeTable[programmeCourse.typeId];
-            let typeId = programmeCourse.typeId;
-            let typeObj = typeTable.find((type) => type.typeId === typeId);
-            let type = typeObj ? typeObj.type : null;
-            if (type) {
-                //console.log("!!!!!",course.courseCode);
-                let credits = course.credits;
-                creditRequirements[type] -= credits;
-                totalCredits += credits;
-                completedCourses.push(course.courseCode);
+        for (let i = 0; i < studentCourseCodes.length; i++){
+            let course = courses.find((c) => c.courseCode === studentCourseCodes[i]);
+            let programmeCourse = programmeCourses.find((c) => c.courseCode === studentCourseCodes[i] && c.typeId === typeId);
+            if(creditRequirements[creditType]<=0){
+                break;
             }
-
+            if(programmeCourse && !completedCourses.includes(programmeCourse.courseCode)){
+                let credits = course.credits;
+                completedCourses.push(course.courseCode);
+                creditRequirements[creditType] -= credits;
+                // console.log("both: ", programmeCourse);
+            }
         }
+
     }
+
+    // for (let i = 0; i < studentCourseCodes.length; i++) {
+    //     let course = courses.find((c) => c.courseCode === studentCourseCodes[i]);
+    //     let programmeCourse = programmeCourses.find((c) => c.courseCode === studentCourseCodes[i]);
+
+    //     if (programmeCourse) {
+
+    //         //let type = typeTable[programmeCourse.typeId];
+    //         let typeId = programmeCourse.typeId;
+    //         let typeObj = typeTable.find((type) => type.typeId === typeId);
+    //         let type = typeObj ? typeObj.type : null;
+    //         if (type) {
+    //             //console.log("!!!!!",course.courseCode);
+    //             let credits = course.credits;
+    //             creditRequirements[type] -= credits;
+    //             totalCredits += credits;
+    //             completedCourses.push(course.courseCode);
+    //         }
+
+    //     }
+    // }
 
     let remainingRequirements = [];
     for (let type in creditRequirements) {
